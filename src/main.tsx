c59d0311b3d3a5026cd53fb79db180b2340eae3f
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-08-17 19:19:34 -0400

    react-router/-dom npm installed, and developer dependencies @types/....

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 import { App } from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode></React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
